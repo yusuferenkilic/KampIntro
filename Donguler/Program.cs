766ff3dd135ee69c26e740d07a9ffb086913bb17
yusuf <yusuf@DESKTOP-L8N8OGD>
2021-09-12 19:10:40 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+
+            string[] kurslar = new string[] { 
+                "Yazılım Geliştirici Yetiştirme Kampı", 
+                "Programlamaya Başlangıç İçin Temel Kurs", 
+                "Java", 
+                "Python",
+                "C#" };            
+
+            for (int i = 0; i < kurslar.Length; i++) //(i=i+2)  (i+=2)  i++ =} tek tek artır
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGrisiYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir Butonu"");
+            }
+
+            if (sistemeGrisiYapmisMi == true)
+            {
+                Console.WriteLine("Ayarlar");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
